febe48ff9841e4be850ea1ea2864d81f1f22dbc5
Amaan <amaan14999@gmail.com>
2023-07-10 00:39:35 +0530

    contact page up

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
-const blob = document.getElementById("blob");
+// const blob = document.getElementById("blob");
 
-window.onpointermove = (event) => {
-  const { clientX, clientY } = event;
+// window.onpointermove = (event) => {
+//   const { clientX, clientY } = event;
 
-  blob.animate(
-    {
-      left: `${clientX}px`,
-      top: `${clientY}px`,
-    },
-    { duration: 3000, fill: "forwards" }
-  );
-};
+//   blob.animate(
+//     {
+//       left: `${clientX}px`,
+//       top: `${clientY}px`,
+//     },
+//     { duration: 3000, fill: "forwards" }
+//   );
+// };
+
+const ename = document.getElementById("name");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+function sendEmail() {
+  let ebody = `<b>Name:</b> ${ename.value} <br> <b>Email:</b> ${email.value} <br> <b>Message:</b> ${message.value}`;
+
+  Email.send({
+    SecureToken: "0ab09774-3d4a-4b8d-96b6-6b3ff3e51866",
+    To: "amaan14999@gmail.com",
+    From: "amaanlmc@gmail.com",
+    Subject: "NEW MESSAGE FROM " + email.value,
+    Body: ebody,
+  }).then((message) => alert("Sent"));
+}
